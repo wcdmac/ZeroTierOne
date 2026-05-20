2d9ca11d82df91bcbbf9457fac4ee0c3fa825c89
wcdmac <wcdmac@hotmail.com>
2026-05-20 16:00:10 +0800

    fix: use [NEProvider main] public ObjC API instead of private NEProviderMain C function

--- ios/ZeroTierOne/ZeroTierTunnel/main.m
+++ ios/ZeroTierOne/ZeroTierTunnel/main.m
@@ -1,7 +1,5 @@
 #import <Foundation/Foundation.h>
-#import <dlfcn.h>
-#import <stdio.h>
-#import <string.h>
+#import <NetworkExtension/NetworkExtension.h>
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
@@ -9,29 +7,14 @@ int main(int argc, char *argv[]) {
         NSLog(@"[ZT-Tunnel] PID: %d", getpid());
         NSLog(@"[ZT-Tunnel] argc: %d", argc);
 
-        void *neHandle = dlopen("/System/Library/Frameworks/NetworkExtension.framework/NetworkExtension", RTLD_NOW);
-        if (neHandle) {
-            NSLog(@"[ZT-Tunnel] NetworkExtension framework loaded successfully");
+        if ([NEProvider respondsToSelector:@selector(main)]) {
+            NSLog(@"[ZT-Tunnel] Calling [NEProvider main]...");
+            [NEProvider main];
+            NSLog(@"[ZT-Tunnel] [NEProvider main] returned");
         } else {
-            NSLog(@"[ZT-Tunnel] FATAL: Failed to load NetworkExtension framework: %s", dlerror());
+            NSLog(@"[ZT-Tunnel] WARNING: NEProvider does not respond to +main selector!");
+            [[NSRunLoop currentRunLoop] run];
         }
-
-        if (neHandle) {
-            typedef int (*NEProviderMainFunc)(int, char **);
-            NEProviderMainFunc neProviderMain = (NEProviderMainFunc)dlsym(neHandle, "NEProviderMain");
-            if (neProviderMain) {
-                NSLog(@"[ZT-Tunnel] NEProviderMain found, calling it...");
-                int result = neProviderMain(argc, argv);
-                NSLog(@"[ZT-Tunnel] NEProviderMain returned: %d", result);
-                return result;
-            } else {
-                NSLog(@"[ZT-Tunnel] FATAL: NEProviderMain not found in NetworkExtension framework!");
-                NSLog(@"[ZT-Tunnel] dlerror: %s", dlerror());
-            }
-        }
-
-        NSLog(@"[ZT-Tunnel] Falling back to NSRunLoop...");
-        [[NSRunLoop currentRunLoop] run];
     }
     return 0;
 }

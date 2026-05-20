97e62f7eac6e82bc5bc410ed3d20252329b1abb0
wcdmac <wcdmac@hotmail.com>
2026-05-20 11:12:49 +0800

    fix: add -Xlinker -isysroot to force iOS SDK sysroot for linker

--- ios/build-ios.sh
+++ ios/build-ios.sh
@@ -66,6 +66,8 @@ swiftc \
     -module-name ZeroTierTunnel \
     -parse-as-library \
     -import-objc-header "$PROJECT_DIR/ZeroTierOne/ZeroTierTunnel/ZeroTierTunnel-Bridging-Header.h" \
+    -Xlinker -isysroot \
+    -Xlinker "$SDK" \
     -Xlinker -force_load \
     -Xlinker "$ROOT_DIR/libzerotiercore-ios.a" \
     "$OBJ_DIR/ZTNodeBridge.o" \

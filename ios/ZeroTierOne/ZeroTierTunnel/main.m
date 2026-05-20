71376a7475d09172f60f11d707a68bc3179e136b
wcdmac <wcdmac@hotmail.com>
2026-05-20 11:07:14 +0800

    fix: declare NEProviderMain as extern in main.m for NetworkExtension entry point

--- ios/ZeroTierOne/ZeroTierTunnel/main.m
+++ ios/ZeroTierOne/ZeroTierTunnel/main.m
@@ -1,4 +1,6 @@
-#import <NetworkExtension/NetworkExtension.h>
+#import <Foundation/Foundation.h>
+
+extern int NEProviderMain(int argc, char **argv) __attribute__((visibility("default")));
 
 int main(int argc, char *argv[]) {
     return NEProviderMain(argc, argv);
